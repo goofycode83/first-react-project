96bbb344c420278440c107be432d966a91b42c30
Kaleab Fekade <kaleab83@gmail.com>
2023-11-13 16:04:37 -0500

    got data.js/linked file and map over data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppies)
   return (
     <>
       <div>
+        {
+          puppies.map((puppies) => {
+            return <p>{puppies.name}</p>
+          })
+        }
       </div>
     </>
   )
