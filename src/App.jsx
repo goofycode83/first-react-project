d14011084c6550e5e1f8d019c897c5f49d439d36
Kaleab Fekade <kaleab83@gmail.com>
2023-11-14 10:41:27 -0500

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,18 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: " ,puppyList)
   const featuredPup = puppies.find((pup)=> pup.id === featPupId)
-            console.log(featuredPup)
   return (
     <>
       { featPupId && (
         <div>
           <h2>{featuredPup.name}</h2>
           <ul>
-            <li>Age: {featuredPup.age}</li>
+            <li >Age: {featuredPup.age}</li>
             <li>Email: {featuredPup.email}</li>
           </ul>
         </div>
         )}
-
       <div className="App">
         {
           puppies.map((puppy) => {
